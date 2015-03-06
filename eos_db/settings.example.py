0fd5a2171be5e5b4afe0f42f39dd6254186fefa2
Ben Collier <bmcollier@gmail.com>
2015-03-06 09:37:15 +0000

    Adding commentary to settings example.

--- eos_db/settings.example.py
+++ eos_db/settings.example.py
@@ -1,3 +1,6 @@
+"""This is an example settings file. Change the username and password to your
+database username and password of choice."""
+
 class DBDetails():
     username = 'databasedude'
     password = 'god'
